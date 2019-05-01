8ff7bac626f2ed9f4db2766dace797d2a2ed30db
KiranShetty <kirumars@gmail.com>
2019-05-01 21:29:25 +0530

    Initial commit

---
+++ src/main/java/demo/DemoClass1.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class DemoClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is demo class for Devopstraining");
+	}
+
+}
