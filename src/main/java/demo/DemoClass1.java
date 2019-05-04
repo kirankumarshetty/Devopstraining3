db2fccba764ee31cc9cba878b80702c955cfcf89
kirankumarshetty <kirumars@gmail.com>
2019-05-04 17:07:04 +0530

    updated juint and few changes on demo class

--- src/main/java/demo/DemoClass1.java
+++ src/main/java/demo/DemoClass1.java
@@ -1,8 +1,11 @@
 package demo;
 
+import org.junit.Test;
+
 public class DemoClass1 {
 
-	public static void main(String[] args) {
+	@Test
+	public void testMethod() {
 		// TODO Auto-generated method stub
 		System.out.println("This is demo class for Devopstraining");
 		// some change
