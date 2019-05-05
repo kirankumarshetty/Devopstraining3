fe0d9fc1682a3d38c567f402936e3e6ea443f5ef
kirankumarshetty <49029516+kirankumarshetty@users.noreply.github.com>
2019-05-05 21:55:00 +0530

    update democlass1

--- src/main/java/demo/DemoClass1.java
+++ src/main/java/demo/DemoClass1.java
@@ -7,7 +7,7 @@ public class DemoClass1 {
 	public static void main(String []args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is demo class for Devopstraining");
-		// some change on demo class
+		// some change on demo class1
 	}
 
 }
